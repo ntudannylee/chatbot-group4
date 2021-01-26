35d1aedd136ee8122ca78082edbf675ad78615b7
ericlin03 <you@example.com>
2021-01-27 00:04:43 +0800

    QnAMaker migrate

--- config.py
+++ config.py
@@ -10,3 +10,7 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
+    QNA_KNOWLEDGEBASE_ID = os.environ.get("QnAKnowledgebaseId", "aa9a8da0-a395-4047-9c3d-9838fc1d9a6c")
+    QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "cd7b25b5-973c-436f-a7f6-887047b6dd3a")
+    QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "https://restaurant-recom-qna.azurewebsites.net/qnamaker")
