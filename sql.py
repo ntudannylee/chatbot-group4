67ac9716bbc4a0f10faa154c6b750a214d9f8b59
林展宏 <linzhanhong@linzhanhongdeMacBook-Pro.local>
2021-01-29 03:47:39 +0800

    5

--- sql.py
+++ sql.py
@@ -70,15 +70,12 @@ database = 'restaurant_DB'
 username = 'rest-admin'
 password = 'Chatbot4'   
 # driver= '{ODBC Driver 17 for SQL Server}'
-try:
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-except Exception as e:
-    error_class = e.__class__.__name__ #取得錯誤類型
-    detail = e.args[0] #取得詳細內容
-    cl, exc, tb = sys.exc_info() #取得Call Stack
-    lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
-    fileName = lastCallStack[0] #取得發生的檔案名稱
-    lineNum = lastCallStack[1] #取得發生的行號
-    funcName = lastCallStack[2] #取得發生的函數名稱
-    errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
-    print(errMsg)
+# try:
+print(pyodbc.dataSources())
+print(pyodbc.drivers())
+
+drivers = [item for item in pyodbc.drivers()]
+print(drivers)
+driver = drivers[-1]
+print("driver:{}".format(driver))
+cnxn = pyodbc.connect('DRIVER={%s};SERVER='%(driver)+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
