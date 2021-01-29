3476a3b19277920e657890063715295d97318c69
ericlin03 <you@example.com>
2021-01-29 16:27:43 +0800

    優化歷史紀錄與我的最愛

--- history.py
+++ history.py
@@ -14,18 +14,21 @@ class history:
     def add_history(self, user_id, restaurant_name):
         counter = self.db_func.DB_query('SELECT counter FROM user_info WHERE ID=\'' + user_id + '\'')
         res = self.db_func.DB_query('SELECT recently FROM user_info WHERE ID=\'' + user_id + '\'')
+        if (len(restaurant_name) > 1):
+            output = ''
+            for i in range(len(restaurant_name)):
+                output += restaurant_name[i]
         if (res[0] is not None):
             history = res[0].split(' ')
-            if (restaurant_name not in history):
-                history[counter] = restaurant_name
+            if (output not in history):
+                history[counter] = output
                 put_back = ''
                 for i in range(len(history)):
-                    put_back += history[i]
+                    put_back += history[i] + ' '
                 counter = (counter + 1) % 10
-                self.db_func.DB_insert('UPDATE user_info SET recently=\'' + put_back + '\' WHERE ID=\'' + user_id + '\';')
+                self.db_func.DB_insert('UPDATE user_info SET recently=\'' + put_back + ' ' + output + '\' WHERE ID=\'' + user_id + '\';')
                 self.db_func.DB_insert('UPDATE user_info SET counter=\'' + counter + '\' WHERE ID=\'' + user_id + '\';')
                 self.db_func.DB_commit()
         else:
-            print(restaurant_name)
-            self.db_func.DB_insert('UPDATE user_info SET recently=\'' + restaurant_name + '\' WHERE ID=\'' + user_id + '\';')
+            self.db_func.DB_insert('UPDATE user_info SET recently=\'' + output + '\' WHERE ID=\'' + user_id + '\';')
             self.db_func.DB_commit()
