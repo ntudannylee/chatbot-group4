759fad99caf15e47403720debc4d860a76d62178
Kevinkang0211 <45477381+Kevinkang0211@users.noreply.github.com>
2021-01-29 12:34:19 +0800

    重新clone後 再新增ig 爬蟲

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ from history import add_history, get_history
 from blogcrawler import blogcrawler
 from linebot.models.sources import SourceUser
 from azure.cognitiveservices.language.luis.authoring import LUISAuthoringClient
+from igcrawler import crawl
 
 
 class MyBot(ActivityHandler):
